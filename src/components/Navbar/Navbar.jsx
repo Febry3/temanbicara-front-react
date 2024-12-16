4c94883bda98209e6d967eb6c528378171bde735
Fehehehe <febryt22@gmail.com>
2024-12-17 00:08:19 +0700

    feat: adding toggle sidebar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,71 @@
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import logo from '../../assets/telogo.png';
+import { useState } from 'react';
+
+
+
+
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(true);
+
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
         <>
-            <aside className="sidebar">
-                <img className='sidebar-logo' src={logo} alt="Logo" />
-                <ul className="sidebar-links">
-                    <li>
-                        <Link to="/"> <i class="bi bi-house"></i> Dashboard</Link>
-                    </li>
-                    <li>
-                        <Link to="/about"> <i class="bi bi-journal-text"></i> Article</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact"><i class="bi bi-clipboard"></i> Quiz</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact"> <i class="bi bi-person"></i> Counselor</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact"><i class="bi bi-bar-chart"></i> Report</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact"><i class="bi bi-person-gear"></i> Admin</Link>
-                    </li>
-                </ul>
+            <button className={isOpen ? "sidebar-button" : "sidebar-button-closed"} onClick={toggleSidebar}>{isOpen ? <i class="bi bi-caret-left-fill"></i> : <i class="bi bi-caret-right-fill"></i>}</button>
+            <aside className={isOpen ? "sidebar" : "sidebar-closed"}>
+                {
+                    isOpen ?
+                        <>
+                            <img className='sidebar-logo' src={logo} alt="Logo" />
+                            <ul className="sidebar-links">
+                                <li>
+                                    <Link to="/"> <i class="bi bi-house"></i> Dashboard</Link>
+                                </li>
+                                <li>
+                                    <Link to="/about"> <i class="bi bi-journal-text"></i> Article</Link>
+                                </li>
+                                <li>
+                                    <Link to="/contact"><i class="bi bi-clipboard"></i> Quiz</Link>
+                                </li>
+                                <li>
+                                    <Link to="/contact"> <i class="bi bi-person"></i> Counselor</Link>
+                                </li>
+                                <li>
+                                    <Link to="/contact"><i class="bi bi-bar-chart"></i> Report</Link>
+                                </li>
+                                <li>
+                                    <Link to="/contact"><i class="bi bi-person-gear"></i> Admin</Link>
+                                </li>
+                            </ul>
+                        </> :
+                        <>
+                            <img className='sidebar-logo' src={logo} alt="Logo" />
+                            <ul className="sidebar-links">
+                                <li>
+                                    <Link to="/"> <i class="bi bi-house"></i></Link>
+                                </li>
+                                <li>
+                                    <Link to="/about"> <i class="bi bi-journal-text"></i></Link>
+                                </li>
+                                <li>
+                                    <Link to="/contact"><i class="bi bi-clipboard"></i></Link>
+                                </li>
+                                <li>
+                                    <Link to="/contact"> <i class="bi bi-person"></i></Link>
+                                </li>
+                                <li>
+                                    <Link to="/contact"><i class="bi bi-bar-chart"></i></Link>
+                                </li>
+                                <li>
+                                    <Link to="/contact"><i class="bi bi-person-gear"></i></Link>
+                                </li>
+                            </ul>
+                        </>
+                }
             </aside>
         </>
     );
